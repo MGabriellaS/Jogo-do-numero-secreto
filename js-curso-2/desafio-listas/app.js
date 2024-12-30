8e481121ab705151fc2447625b96f89e63054f1f
MGSS <mgss@Gabriella-Santos-MacBook-Pro.local>
2024-12-30 15:00:00 -0300

    add changes

--- js-curso-2/desafio-listas/app.js
+++
@@ -1,7 +0,0 @@
-let listaGenerica = [];
-let linguagensDeProgramacao = [ 'JavaScript','C','C++', 'Kotlin' , 'Python'];
-linguagensDeProgramacao.push( 'Java', 'Ruby' , 'GoLang');
-let listaNomes =['Gabriella', 'Maria', 'Joana'];
-console.log(listaNomes[0]);
-console.log(listaNomes[1]);
-console.log(listaNomes[2]);
